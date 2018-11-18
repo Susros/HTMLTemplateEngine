aeb09a3e7a557895c74a187e7b33a38d7403381c
Susros <kelvin.susros@gmail.com>
2018-11-18 17:27:11 +1100

    Test

---
+++ test/HTMLTemplateTest.php
@@ -0,0 +1,33 @@
+<?php
+
+include "../src/HTMLTemplateData.php";
+
+include "../src/HTMLTemplate.php";
+use HTMLTemplateEngine\HTMLTemplate;
+
+$tpl = file_get_contents("test.tpl");
+
+$t = new HTMLTemplate($tpl);
+
+$t->pushVar("done", "SUCCESS");
+$t->pushVar("description", "Small description make things a lot difficult.");
+$t->pushVar("val", array(1, 2, 3, 4));
+$t->pushVar("op", array("one", "two", "three", "four"));
+$t->execute();
+
+echo "<pre>" . htmlspecialchars($t) . "</pre>";
+
+
+
+// Debug
+/*foreach($this->templateData as $v) {
+	echo "<b>Template String:</b>". htmlspecialchars($v->getTemplateString()) . "<br>";
+	echo "<b>HTML String:</b>". htmlspecialchars($v->getHtmlString()) . "<br>";
+	echo "<b>Function:</b>" . $v->getFunction() . "<br>";
+	echo "<b>Loop Time:</b>" . $v->getLoopTime() . "<br>";
+	echo "<b>Variables:</b>";
+	echo "<pre>"; print_r($v->getVars()); echo "</pre>";
+	echo "<hr>";
+} */
+
+?>
---
+++ test/test.tpl
@@ -0,0 +1,9 @@
+<div>
+{#loop(3):<p>{$msg:"Hello World"}</p>;} Testing {$done:"DONE"}
+</div>
+
+<p>{$description}</p>
+
+<select>
+{#dup:<option value="{$val}">{$op}</option>;}
+</select>
